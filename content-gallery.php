325aa15c6d4729874f6543c7ea6f56d0a054c0c4
ChayaH99 <77052198+ChayaH99@users.noreply.github.com>
2021-02-03 21:16:37 +0200

    Add files via upload

---
+++ content-gallery.php
@@ -0,0 +1,28 @@
+<div class="grid gallery container flex flex-wrap center ph4-l">
+  <?php $images = get_sub_field('gallery'); ?>
+  <?php foreach($images as $image) : ?>
+   
+    <div class="gallery-image ph3 mb4 <?php the_field('image_width', $image['id']); ?>">
+    
+    
+      <?php echo wp_get_attachment_image($image['id'], 'full'); ?>
+      
+      <!-- here we assign our caption to a variable called $caption -->
+      <?php $caption = wp_get_attachment_caption($image['id']); ?>
+      
+      <!-- if the caption is not empty, we render it onto the page -->
+      <?php if(!empty($caption)) : ?> 
+        <p class="caption archivo-regular f5 o-50 pt3 mv0">
+          <!-- here we grab the caption by the attachment id -->
+          <?php echo $caption; ?>
+        </p>
+      <?php endif; ?>
+
+    </div>
+
+  <?php endforeach; ?>
+
+
+</div>
+
+  
